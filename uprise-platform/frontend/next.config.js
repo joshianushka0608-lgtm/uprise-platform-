e4f6ea89c7e81564e6accdd45f8c5aad7a8da57d
lilylikemusic <lilyd687@gmail.com>
2026-04-28 14:02:56 +0530

    remove broken rewrite

--- uprise-platform/frontend/next.config.js
+++ uprise-platform/frontend/next.config.js
@@ -4,13 +4,5 @@ const nextConfig = {
   images: {
     domains: ['localhost', 'api.corntub.xyz'],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://uprise-backend.up.railway.app/:path*',
-      },
-    ];
-  },
 };
 module.exports = nextConfig;
